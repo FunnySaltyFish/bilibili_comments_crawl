e01f5ac5d4ead15100fd18efc022302718e3e739
FunnySaltyFish <3036548996@qq.com>
2023-08-29 22:52:34 +0800

    完成爬取单个视频的所有逻辑

--- bilibili_comments_crawler.py
+++ bilibili_comments_crawler.py
@@ -5,6 +5,7 @@ import httpx
 import json
 from typing import List, Dict, Any, TypeAlias
 from async_pool import AsyncPool
+import os
 
 JSON_TYPE: TypeAlias = Dict[str, Any]
 COMMON_HEADERS = {
@@ -13,6 +14,13 @@ COMMON_HEADERS = {
 }
 
 pool = AsyncPool(maxsize=16)
+
+def save_obj(obj: JSON_TYPE, filename: str):
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(obj, f, ensure_ascii=False, indent=2)
 
 async def get_html(url: str, params: Dict = None, headers: Dict = None, timeout: int = 30):
     try:
@@ -53,7 +61,8 @@ async def get_one_page_reply(oid: int, page: int, r_root: int):
     url = "https://api.bilibili.com/x/v2/reply/reply"
     text = await get_html(url, params, COMMON_HEADERS)
     obj = json.loads(text)
-    print("爬取 r_root: {}, page: {} 完毕".format(r_root, page))
+    if page % 10 == 0:
+        print("---- 爬取 r_root: {}, page: {} 完毕".format(r_root, page))
     await asyncio.sleep(0.01)
     return obj["data"]["replies"]
 
@@ -62,7 +71,8 @@ async def get_reply(oid: int, r_root: int, total_count: int):
     total_page = total_count // 20 + 1
     replies = []
     for page in range(1, total_page + 1):
-        print("正在爬取 r_root: {}, page: {}".format(r_root, page))
+        if page % 10 == 0:
+            print("---- 正在爬取 r_root: {}, page: {}".format(r_root, page))
         pool.submit(get_one_page_reply(oid, page, r_root), callback=lambda future: replies.extend(future.result()))
     pool.wait()
     return replies
@@ -117,8 +127,10 @@ def build_conv_from_replies(root_reply: JSON_TYPE, replies: List[JSON_TYPE]) -> 
 
 
     
-async def crawl_bilibili_comments(oid: int):
-    """https://api.bilibili.com/x/v2/reply/main?csrf=9b261f0d10434bbefd17d7f4bd8247f2&mode=3&oid=275076584&pagination_str=%7B%22offset%22:%22%7B%5C%22type%5C%22:1,%5C%22direction%5C%22:1,%5C%22session_id%5C%22:%5C%221733821037173918%5C%22,%5C%22data%5C%22:%7B%7D%7D%22%7D&plat=1&type=1
+async def crawl_one_page_video(oid: int, page: int):
+    """
+    爬取一个视频一页的评论
+    https://api.bilibili.com/x/v2/reply/main?csrf=9b261f0d10434bbefd17d7f4bd8247f2&mode=3&oid=275076584&pagination_str=%7B%22offset%22:%22%7B%5C%22type%5C%22:1,%5C%22direction%5C%22:1,%5C%22session_id%5C%22:%5C%221733821037173918%5C%22,%5C%22data%5C%22:%7B%7D%7D%22%7D&plat=1&type=1
     type	num	评论区类型代码	必要	类型代码见表
     oid	num	目标评论区 id	必要	
     sort	num	排序方式	非必要	默认为0
@@ -133,41 +145,50 @@ async def crawl_bilibili_comments(oid: int):
     pn	num	页码	非必要	默认为1
     """
 
-    page = 1
-    print("开始爬取评论")
+    print("-- 开始爬取视频 {} 的第 {} 页评论".format(oid, page))
     obj = await get_one_page(oid, page)
-    # with open("bilibili_comments_full.json", "w", encoding="utf-8") as f:
-    #     json.dump(obj, f, ensure_ascii=False, indent=4)
 
     video_replies = obj["data"]["replies"]
-    total_page: int = obj["data"]["cursor"]["all_count"] // 20 + 1
-    # 获取所有评论
-    # 并发 8个协程
 
-    all_comment_replies = {
+    for root_reply in video_replies:
+        comment_replies: Dict[str, Any] = await get_reply(oid, root_reply["rpid"], root_reply["rcount"])
+        conversations = build_conv_from_replies(root_reply, comment_replies)
+        save_obj(conversations, f"data/video_{oid}/page_{page}/rpid_{root_reply['rpid']}_convs.json")
+        print("--- 保存 rpid 为 {:12d} 的评论并构建对话完毕，共 {:6d} 条".format(root_reply["rpid"], len(conversations)))
+        await asyncio.sleep(0.1)
 
+async def crawl_one_video(oid: int):
+    """
+    爬取一个视频的所有评论
+    """
+    print("- 开始爬取视频 {} 的评论".format(oid))
+    """
+    https://api.bilibili.com/x/v2/reply/count
+
+    请求方式：GET
+
+    url参数：
+
+    参数名	类型	内容	必要性	备注
+    type	num	评论区类型代码	必要	类型代码见表
+    oid	num	目标评论区 id	必要	
+    """
+    url = "https://api.bilibili.com/x/v2/reply/count"
+    params = {
+        "type": 1,
+        "oid": oid
     }
-
-    def callback(future: asyncio.Future):
-        pass
-
-    
-    # for page in range(1, total_page + 1):
-    #     pool.submit(get_one_page(oid, page))
-
-
-    comment_replies: Dict[str, Any] = await get_reply(oid, video_replies[0]["rpid"], video_replies[0]["rcount"])
-
-    # print(obj)
-    # with open("bilibili_comments_replies.json", "w", encoding="utf-8") as f:
-    #     json.dump(comment_replies, f, ensure_ascii=False, indent=4)
-    root_reply = video_replies[0]
-    conversations = build_conv_from_replies(root_reply, comment_replies)
-    with open("bilibili_comments_replies_convs.json", "w", encoding="utf-8") as f:
-        json.dump(conversations, f, ensure_ascii=False, indent=4) 
+    text = await get_html(url, params, COMMON_HEADERS)
+    obj = json.loads(text)
+    total_page: int = obj["data"]["count"] // 20 + 1
+    print("- 视频 {} 一共有 {} 页评论".format(oid, total_page))
+    for page in range(1, total_page + 1):
+        await crawl_one_page_video(oid, page)
+        print("-- 爬取视频 {} 的第 {} 页评论完毕".format(oid, page))
+        await asyncio.sleep(0.1)
 
 async def main():
-    await crawl_bilibili_comments(2)
+    await crawl_one_video(2)
 
 if __name__ == "__main__":
     asyncio.run(main())
